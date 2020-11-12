b512799b73988bb9f43c20e7259894b03deef462
Georg Wölflein <georgw7777@gmail.com>
2020-11-12 23:56:20 +0000

    Basic API implementation

---
+++ api/app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, File
+import cv2
+import numpy as np
+from enum import Enum
+import chess
+from pydantic import BaseModel
+
+from chesscog import __version__ as chesscog_version
+from chesscog.recognition import ChessRecognizer
+
+from __version__ import __version__ as api_version
+
+app = FastAPI()
+recognizer = ChessRecognizer()
+
+
+class Turn(str, Enum):
+    WHITE = "white"
+    BLACK = "black"
+
+
+class Prediction(BaseModel):
+    fen: str
+    lichess_url: str
+
+
+@app.get("/version")
+def version():
+    return {"api": api_version, "chesscog": chesscog_version}
+
+
+@app.post("/predict", response_model=Prediction)
+def read_item(turn: Turn = Turn.WHITE, file: bytes = File(...)):
+    buffer = np.frombuffer(file, np.uint8)
+    img = cv2.imdecode(buffer, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    board = recognizer.predict(img, turn == Turn.WHITE)
+    fen = board.board_fen()
+    return Prediction(fen=fen,
+                      lichess_url=f"https://lichess.org/editor/{fen}")
